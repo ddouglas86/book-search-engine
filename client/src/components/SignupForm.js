b049d26f7dfde19a2d64ebd306ac4df894770507
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-03 19:28:03 -0600

    added imports for usemutation and add_user mutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations'
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
