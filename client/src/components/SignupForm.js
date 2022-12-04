491688a6d5366f2ffc847b89d385ac30956d7b5e
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-03 19:30:36 -0600

    added adduser hook

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -12,6 +12,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
