dfa458ca6c9c30692615a0084f8fc2ea5f65c221
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-04 02:41:39 -0600

    updated to get save to work properly

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -69,15 +69,15 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook({
+      await saveBook({
         variables: {
           newBook: bookToSave,
         },
       });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
