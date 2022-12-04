97b175ed8f05d9d681cb50f7ab2cc7bdd198bb20
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-03 18:36:57 -0600

    added removebook mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -65,3 +65,21 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
