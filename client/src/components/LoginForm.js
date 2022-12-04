929c46841aaefc195d9b35c3a583aadbb3558b5f
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-03 19:42:23 -0600

    added missing loginUser hook

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -10,6 +10,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
