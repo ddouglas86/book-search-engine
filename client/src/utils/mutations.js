0b25b22388a20b287235d6c6ff1a10a77336c0df
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-03 18:24:50 -0600

    added initial mutation file. inmport gql and loginuser mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation loginUser(
+    $email: String!
+    $password: String!
+  ) {
+    loginUser(
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
