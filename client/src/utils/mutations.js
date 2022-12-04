053ff29e16ee00954bc7bcb34f7ef2656ffa41fe
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-03 18:34:48 -0600

    added adduser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -17,3 +17,33 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+    }
+  }
+`;
