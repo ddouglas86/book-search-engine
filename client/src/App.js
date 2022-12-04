910f283055f8fc3e28e2dc789fb7fc9c0e508d6a
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-03 18:42:53 -0600

    added imports for apolloclient, provider, etc

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+
 
 function App() {
   return (
