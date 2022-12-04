ec2d797294ceef4a5d63a202a6f9daf62ba39577
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-12-03 19:37:35 -0600

    updated try catch with mutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -30,15 +30,10 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
